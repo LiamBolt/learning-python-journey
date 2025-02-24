ebf3e0c3bc70324b8b2accde3b6000626d11e98e
LiamBolt <amwineliam@gmail.com>
2025-02-24 11:05:01 +0300

    All actions on the routes require an authentication

--- FASTAPI_learning/app/oauth2.py
+++ FASTAPI_learning/app/oauth2.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 
 
@@ -10,7 +11,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = "f4b3f6b7ee6e7b8b7b7ae3e3f4"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_access_token(data: dict, ):
     to_encode = data.copy()
@@ -37,7 +38,10 @@ def verify_access_token(token: str, credentials_exception):
         
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
